5a396ae7b7018935caef73a584c1f70fa0ed7709
nbaugh1 <nbaugh1@gmail.com>
2019-12-31 02:07:11 -0500

    add react bootstrap and bootstrap, stylesheet and other dependencies for styling

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css';
 import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-spa";
 import config from "./auth_config.json";
 import history from "./utils/history";
+import './custom.scss'
 
 // A function that routes the user to the right place
 // after login
