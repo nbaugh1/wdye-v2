58c860e0e5dab3195998ae0641e7ec55b0911422
nbaugh1 <nbaugh1@gmail.com>
2020-01-07 12:29:48 -0500

    fix bug in secrets.yml file (incorrect format/syntax) and add ActiveAdmin resources for Visits and Restaurants

---
+++ app/admin/restaurants.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Restaurant do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :cuisine, :yelp_id, :location, :price_range
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :cuisine, :yelp_id, :location, :price_range]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/visits.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Visit do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :visit_date, :user_id, :restaurant_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:visit_date, :user_id, :restaurant_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
