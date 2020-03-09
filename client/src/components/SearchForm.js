db4d6bbe2f61c9abae036a12d8dea00b10afeabe
nbaugh1 <nbaugh1@gmail.com>
2020-03-08 20:12:14 -0400

    moving depedences to client

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { FormLabel, Form, Button } from 'react-bootstrap'
+import axios from 'axios'
 
 const SearchForm = props => {
   const [searchValue, setSearchValue] = useState('')
+  const [restaurantResults, setRestaurantResults] = useState('')
 
   const handleSearchInputChange = e => {
     setSearchValue(e.target.value)
@@ -12,9 +14,15 @@ const SearchForm = props => {
     setSearchValue('')
   }
 
-  const handleSubmit = () => {
+  const handleSubmit = e => {
     e.preventDefault()
-    props.search(searchValue)
+    axios
+      .get('https://api.yelp.com/v3/businesses/{searchValue}')
+      .then(response => {
+        const restaurants = response.data
+        this.setRestaurantResults({ restaurants })
+        console.log(restaurants)
+      })
     resetSearchInput()
   }
 
